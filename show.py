8dd86e5b6fd4fbbdc9b6bc654d6c6e0548df75b3
jingpengwu <jingpeng.wu@gmail.com>
2015-07-14 14:38:51 -0400

    add aws, enhance the volume compare code using Nick's code

--- show.py
+++ show.py
@@ -8,39 +8,126 @@ import numpy as np
 import matplotlib.pylab as plt
 
 class CompareVol:
-    def __init__(self, v1, v2, cmap='gray'):
-        self.v1 = v1
-        self.v2 = v2
-        self.Nz = min(v1.shape[0], v2.shape[0])
-        self.z = 0
-        self.cmap = cmap
-        
-    def __show_slice(self):
-        self.ax1.images.pop()
-        self.ax1.imshow(self.v1[self.z,:,:], interpolation='nearest', cmap=self.cmap)
-        self.ax1.set_xlabel( 'first volume: slice {}'.format(self.z) )
+    def __init__(self, vols, cmap='gray'):
 
-        self.ax2.images.pop()
-        self.ax2.imshow(self.v2[self.z,:,:], interpolation='nearest', cmap=self.cmap)
-        self.ax2.set_xlabel( 'second volume: slice {}'.format(self.z) )
+        #zero-padded copies of the volumes
+        self.vols = self.__pad(vols)
+        #Number of slices to display 
+        self.Nz = min([elem.shape[0]-1 for elem in vols])
+        #Current z index
+        self.z = 0
+        #Colormap argument to be passed to imshow (set under 'c' keypress)
+        self.cmap = [cmap for elem in vols]
+        #Whether or not the plot at an index is a color plot
+        self.colorplot = [False for elem in vols]
+
+        #Defining a current index of the vols to be 'selected' for
+        # modification by (for example) adding color
+        self.selected = 0
+
+    def __pad(self, vols):
+        '''Zero-padding a list of input volumes to match by non-z dimensions'''
+        shapes = np.array(
+            [elem.shape for elem in vols]
+            )
+
+        max_shape = np.max(shapes,0)
+
+        pad_vols = [np.zeros((elem.shape[0], max_shape[1], max_shape[2]))
+                    for elem in vols]
+
+        dim_diffs = [(max_shape - elem.shape) / 2
+                    for elem in vols]
+
+        for i in range(len(pad_vols)):
+
+            if all(dim_diffs[i][1:] != 0):
+                pad_vols[i][
+                    :,
+                    dim_diffs[i][1]:-(dim_diffs[i][1]),
+                    dim_diffs[i][2]:-(dim_diffs[i][2])
+                    ] = vols[i]
+            else:
+                pad_vols[i] = vols[i]
+
+        return pad_vols
+
+    def __norm(self, imslice):
+        #subtract the nonzero minimum from each slice
+        nonzero_indices = np.nonzero(imslice)
+        if len(nonzero_indices) > 0 and np.max(imslice) > 1:
+            nonzero_min = np.min(imslice[np.nonzero(imslice)])
+
+            res = np.copy(imslice)
+            res[np.nonzero(res)] = res[np.nonzero(res)] - nonzero_min + 1
+        else: 
+            res = imslice
+        return res
+
+    def __show_slice(self):
+        '''Basic display function'''
+
+        for i in range(1,len(self.axs)+1):
+            ax = self.axs[i-1]
+            ax.images.pop()
+            normed_slice = self.__norm(self.vols[i-1][self.z,:,:])
+            ax.imshow(normed_slice, interpolation='nearest', cmap=self.cmap[i-1])
+            ax.set_xlabel( ' volume {}: slice {}'.format(i,self.z) )
+
         self.fig.canvas.draw()
         
+    def __make_cmap(self, i):
+
+        #(0,0,0) = black
+        plot_colors = np.vstack(((0,0,0), np.random.rand(500,3)))
+        cmap = colors.ListedColormap(plot_colors)
+
+        
+        return cmap
+
     def __press(self, event):
-#        print 'press ' + event.key
+#       print 'press ' + event.key
         if 'down' in event.key and self.z<self.Nz:
             self.z+=1            
         elif 'up' in event.key and self.z>-self.Nz:
             self.z-=1
-        self.__show_slice()        
+        elif 'c' in event.key:
+            #Swap between color display and b&w
+            self.colorplot[self.selected] = not self.colorplot[self.selected]
+            
+            if self.colorplot[self.selected]:
+                new_cmap = self.__make_cmap(self.selected)
+
+                self.cmap[self.selected] = new_cmap
+
+            else:
+                self.cmap[self.selected] = 'gray'
+
+        elif 'v' in event.key:
+            #Display the data values for the given data coordinate
+            xcoord, ycoord = int(event.xdata), int(event.ydata)
+
+            print [vol[self.z, ycoord, xcoord] for vol in self.vols]
+
+
+        elif event.key in ['1','2','3','4','5','6','7','8','9']:
+            #Select a new axis
+            index = int(event.key)
+
+            if index - 1 < len(self.vols):
+                self.selected = index-1
+
+        self.__show_slice()   
         
     def vol_compare_slice(self):   
-        self.fig, (self.ax1, self.ax2) = plt.subplots(1,2, sharey=True)
+        self.fig, self.axs = plt.subplots(1,len(self.vols), sharey=True)
         self.fig.canvas.mpl_connect('key_press_event', self.__press)
-        self.ax1.imshow(self.v1[self.z,:,:], interpolation='nearest', cmap=self.cmap)
-        self.ax1.set_xlabel( 'first  volume: slice {}'.format(self.z) )
-        self.ax2.imshow(self.v2[self.z,:,:], interpolation='nearest', cmap=self.cmap)
-        self.ax2.set_xlabel( 'second volume: slice {}'.format(self.z) )
-	plt.show()
+
+        for i in range(1,len(self.vols)+1):
+            ax = self.axs[i-1]
+            ax.imshow(self.vols[i-1][self.z,:,:], interpolation='nearest', cmap=self.cmap[i-1])
+            ax.set_xlabel( ' volume {0}: slice {1}'.format(i,self.z) )
+        plt.show()
     
 class VolSlider:
     def __init__(self, fname, cmap='gray'):
--- volume_util.py
+++ volume_util.py
@@ -41,3 +41,46 @@ def add_boundary_3D(vol, neighbor = 6):
     vol2[0,:,:] = add_boundary_im(vol[0,:,:])
     vol2[Nz-1,:,:] = add_boundary_im(vol[Nz-1,:,:])
     return vol2
+
+
+def crop(vol, target_shape):
+	'''Crops the input volume to fit to the target shape'''
+	if any([vol.shape[i] < target_shape[i] for i in range(len(target_shape))]	):
+		raise ValueError('volume already smaller that target volume!')
+
+	dim_diffs = (np.array(vol.shape) - np.array(target_shape)) / 2
+
+	#Cropping dimensions one at a time
+	cropped = vol
+	if dim_diffs[0] > 0: #unlikely
+
+		cropped = vol[
+			dim_diffs[0]:-(dim_diffs[0]),
+			:,
+			:
+		]
+
+	if dim_diffs[1] > 0:
+		cropped = cropped[
+			:,
+			dim_diffs[1]:-(dim_diffs[1]),
+			:
+			]
+
+	if dim_diffs[2] > 0:
+		cropped = cropped[
+			:,
+			:,
+			dim_diffs[2]:-(dim_diffs[2])
+		]
+
+	return cropped
+
+def norm(vol):
+	'''Normalizes the input volume to have values between 0 and 1
+	(achieved by factor normalization to the max)'''
+
+	vol = vol - np.min(vol)
+	vol = vol / np.max(vol)
+
+	return vol
