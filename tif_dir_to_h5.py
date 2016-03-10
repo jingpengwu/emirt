3c1a8ca9ee8965bfacbd3094aa0f3863b0e38411
Thomas Macrina <tmacrina@princeton.edu>
2016-03-10 15:05:12 -0500

    Include tif_dir_to_h5.py to compile TIF images in dir to H5 matrix
    Tested with uint24 and uint8 images. Can specify slice parameters
    to crop combined image. Compiles all images, sorted by filename,
    into 3D matrix with same type as the original images. Saves H5 file
    in the same TIF directory.
    
    Use case examples:
    * Compile VAST TIF export into one H5 volume (tested with uint24).
    Crop extra pixels that VAST sometimes adds.
    * Compile & crop TIF stacks from other datasets, like taking DR5
    stack and creating subvolumes (tested with uint8).

---
+++ tif_dir_to_h5.py
@@ -0,0 +1,84 @@
+from PIL import Image
+import numpy as np
+import h5py
+import os
+import sys
+
+def tif_to_array(fn, dtype=np.uint32):
+	"""Open TIF image and convert to numpy ndarray of dtype
+
+	Currently tested for only for uint8 -> uint8, uint32 or uint32 -> uint32
+
+	Args:
+		fn: filename (full path) of the image
+		dtype: data type of the output array
+
+	Returns:
+		An ndarray of dtype
+	"""
+	img = np.array(Image.open(fn))
+	if img.dtype == np.uint32:
+		img = np.dstack((np.zeros(img.shape[:2]+(1,)), img))
+		img = img[:,:, ::-1]
+		img = img.astype(np.uint8).view(dtype)
+		img = img.reshape(img.shape[:2])
+	else:
+		img = img.astype(dtype)
+	return img
+
+def make_array_from_tif_dir(dir):
+	"""Combine all TIF images in directory (sorted) into 3D ndarray
+
+	Args:
+		dir: folder path
+		dtype: data type of the output array
+
+	Returns:
+		An ndarray of dtype with at least 3 dimesnions
+	"""
+	array = None
+	files = os.listdir(dir)
+	files.sort()
+	for file in files:
+		if file.endswith(".tif") or file.endswith(".tiff"):
+			a = tif_to_array(os.path.join(dir, file))
+			if array is None:
+				array = a
+			else:
+				array = np.dstack((array, a))
+	return array
+
+def write_to_h5(fn, arr):
+	"""Write ndarray to H5 file under group "main"
+	"""
+	f = h5py.File(fn, "w")
+	f.create_dataset("/main", data=arr, dtype=arr.dtype)
+	f.close()
+
+def main():
+	"""Make HDF5 3D matrix of TIF images in a sorted directory & cropped
+
+	Args:
+		sys.argv[1]: full path to the TIF image directory
+		sys.argv[2]: filename of the H5 file (placed in the TIF directory)
+		sys.argv[3:7]: x slice start, x slice end, y slice start, y slice end
+	"""
+	dir = os.getcwd()
+	if len(sys.argv) > 1:
+		dir = sys.argv[1]
+	crop = slice(None), slice(None), slice(None)
+	if len(sys.argv) > 6:
+		crop = (slice(int(sys.argv[3]), int(sys.argv[4])),
+					slice(int(sys.argv[5]), int(sys.argv[6])),
+					slice(None))
+	array = make_array_from_tif_dir(dir)
+	array = array[crop]
+	fn = os.path.split(os.getcwd())[-1] + ".h5"
+	if len(sys.argv) > 2:
+		fn = sys.argv[2]
+	if not fn.endswith(".h5"):
+		fn += ".h5"
+	write_to_h5(os.path.join(dir, fn), array)
+
+if __name__ == '__main__':
+	main()
