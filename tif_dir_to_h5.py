4a5e93dc4bfd621b1b2a91e7d650ef984d8e96ef
Thomas Macrina <tmacrina@princeton.edu>
2016-07-05 13:52:33 -0400

    Pass args into main in tif_dir_to_h5.py

--- tif_dir_to_h5.py
+++ tif_dir_to_h5.py
@@ -8,10 +8,9 @@ Make 3D stack of TIF images in a directory (sorted), crop, & save as H5
 Args:
 	sys.argv[1]: full path to the TIF image directory
 	sys.argv[2]: filename of the H5 file (placed in the TIF directory)
-	sys.argv[3:7]: x slice start, x slice end, y slice start, y slice end
 
 Returns:
-	H5 file saved in the same directory
+	H5 file saved in the same directory, in z,y,x order
 """
 
 from PIL import Image
@@ -29,7 +28,7 @@ def tif_to_array(fn):
 		fn: filename (full path) of the image
 
 	Returns:
-		An ndarray of dtype, in z,y,x order
+		An ndarray of dtype
 	"""
 	img = np.array(Image.open(fn))
 	if len(img.shape) == 3:
@@ -52,7 +51,7 @@ def make_array_from_tif_dir(dir):
 	files = os.listdir(dir)
 	files.sort()
 	for file in files:
-		if file.endswith(".tif") or file.endswith(".tiff"):
+		if file.endswith(".tif") or file.endswith(".png"):
 			a = tif_to_array(os.path.join(dir, file))
 			if array is None:
 				array = a
@@ -67,26 +66,18 @@ def write_to_h5(fn, arr):
 	f.create_dataset("/main", data=arr, dtype=arr.dtype)
 	f.close()
 
-def main():
+def main(dir, fn):
 	"""Make HDF5 3D matrix of TIF images in a sorted directory & cropped
 	"""
-	dir = os.getcwd()
-	if len(sys.argv) > 1:
-		dir = sys.argv[1]
-	crop = slice(None), slice(None), slice(None)
-	if len(sys.argv) > 6:
-		crop = (slice(int(sys.argv[3]), int(sys.argv[4])),
-					slice(int(sys.argv[5]), int(sys.argv[6])),
-					slice(None))
 	array = make_array_from_tif_dir(dir)
-	array = array[crop]
 	array = array.transpose((2,1,0))
-	fn = os.path.split(os.getcwd())[-1] + ".h5"
-	if len(sys.argv) > 2:
-		fn = sys.argv[2]
-	if not fn.endswith(".h5"):
-		fn += ".h5"
 	write_to_h5(os.path.join(dir, fn), array)
 
 if __name__ == '__main__':
-	main()
+	if len(sys.argv) > 2:
+		dir = sys.argv[1]
+		fn = sys.argv[2]
+		main(dir, fn)
+	else:
+		print 'Include input directory & output filename'
+		
