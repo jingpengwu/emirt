d5ac70bd9e3247276a28a2f9c99b401a5a27c543
Thomas Macrina <tmacrina@princeton.edu>
2016-07-07 16:27:34 -0400

    Update omnify for dir names

--- omnify_VAST_dir.py
+++ omnify_VAST_dir.py
@@ -22,16 +22,16 @@ import tif_dir_to_h5 as tif2h5
 def main():
 	cwd = os.getcwd()
 	raw = os.path.join(cwd, 'raw')
-	seg = os.path.join(cwd, 'cells_segmentation')
+	seg = os.path.join(cwd, 'cell_segmentation')
 	chann_fn = 'chann.h5'
 	seg_fn = 'seg.h5'
 	print 'Compiling channel data'
 	tif2h5.main(raw, chann_fn)
 	print 'Copying channel data'
 	shutil.move(os.path.join(raw, chann_fn), os.path.join(cwd, chann_fn))
-	print 'Compiling cells segmentation data'
+	print 'Compiling cell segmentation data'
 	tif2h5.main(seg, seg_fn)
-	print 'Copying cells segmentation data'
+	print 'Copying cell segmentation data'
 	shutil.move(os.path.join(seg, seg_fn), os.path.join(cwd, seg_fn))
 	print 'Start omnification'
 	omnify.main(chann_fn, seg_fn)
