ee43f569981f4793e43938abcc4c79217e9e24d9
jingpengw <jingpeng.wu@gmail.com>
2019-05-07 17:49:49 -0400

    initial commit for continue working on it

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <pybind11/pybind11.h>
+#include "seg.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(emirt, m){
+
+    m.doc() = R"pbdoc(
+        emirt
+        ---------
+
+        .. currentmodule:: emirt
+
+        .. autosummary::
+            :toctree: _generate
+
+            expand_segment
+    )pbdoc";
+
+    m.def("expand_segment", &expand_segment, R"pbdoc(
+        Expand segment to shrink the boundary gap
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = VERSION_INFO;
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
---
+++ src/seg.hpp
@@ -0,0 +1,5 @@
+#include <pybind11/pybind11.h>
+#define FORCE_IMPORT_ARRAY
+#include "xtensor-python/pytensor.hpp"
+
+
