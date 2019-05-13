12a130ebb733b16773070f2f77f82b81a334cf55
jingpengw <jingpeng.wu@gmail.com>
2019-05-13 16:18:31 -0400

    update

--- src/seg.hpp
+++ src/seg.hpp
@@ -1,8 +1,17 @@
 #include <pybind11/pybind11.h>
 #include "xtensor-python/pytensor.hpp"
 
-template<typename T>
-class Segmentation{
+template<typename T, std::size_t N, xt::layout_type L>
+class Segmentation: public xt::pytensor<T,N,L>{
     private:
     xt::pytensor<T> seg;
+
+    public:
+
+    // dilate the segmentation with a maximum size
+    template <typename T, std::size_t N, xt::layout_type L>
+    Segmentation<T,N,L> Segmentation<> dilate(std::size_t s)
+    {
+        xt::
+    }
 }
